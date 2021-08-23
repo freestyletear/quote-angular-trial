6082243fa6240ebe814bb3e40cb4ca31ab4d8e30
freestyletear <freestyletear@gmail.com>
2021-08-23 08:33:08 +0300

    add hanges

--- Quotify/src/app/components/quotify/quotify.component.ts
+++ Quotify/src/app/components/quotify/quotify.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../../models/quote';
 export class QuotifyComponent implements OnInit {
 
   quotes: Quote[] = [ /* quote array */
-    new Quote('A friend is someone who knows all about you and still loves you.', 'Elbert Hubbard', 'Lizbeth Wangu', new Date(2020, 1, 5), 5, 0),
-    new Quote('If you can make a woman laugh, you can make her do anything.', 'Marilyn Monroe', 'Lyn Adelaide', new Date(2020, 4, 8), 2, 1),
+    new Quote('I am my better half not yours.', 'krithi', 'Lizbeth Wangu', new Date(2020, 1, 5), 5, 0),
+    new Quote('The lizards were extreamly phylosophical by nature, and often sat together thinking for hours and hours.', 'Oscar Wilde', 'Lyn Adelaide', new Date(2020, 4, 8), 2, 1),
     new Quote('I have no notion of loving people by halves, it is not my nature.', 'Jane Austen', 'Edrea Leslie', new Date(2020, 3, 30), 1, 5),
-    new Quote('It is not a lack of love, but a lack of friendship that makes unhappy marriages.', 'Friedrich Nietzsche', 'Charlene Rose', new Date(2019, 9, 11), 1, 1),
+    new Quote('Hung overs are funn, said no one ever.', 'Laureen Blakery', 'Charlene Rose', new Date(2019, 9, 11), 1, 1),
     new Quote('The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.', ' Bob Marley', 'Albert Austen', new Date(2019, 5, 15), 3, 2),
   ];
 
